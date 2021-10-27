f44bf3fc859aa3f4d062b54a59c4be0207e05649
gongyisheng <Orangepass!2019>
2021-10-27 11:40:06 -0500

    0.3 build main pages with 4 parameters

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Operations from './components/Operations';
 import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-body">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <Operations />
         <Form />
       </header>
     </div>
--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,63 +1,49 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Grid,Input,Spacer,Radio } from '@geist-ui/react'
 
 const Basic = () => (
-    <div>
-        <h1>Anywhere in your app!</h1>
-        <Formik
-            initialValues={{ email: '', password: '' }}
-            validate={values => {
-                const errors = {};
-                if (!values.email) {
-                    errors.email = 'Required';
-                } else if (
-                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                ) {
-                    errors.email = 'Invalid email address';
-                }
-                return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
-                }, 400);
-            }}
-        >
-            {({
-                  values,
-                  errors,
-                  touched,
-                  handleChange,
-                  handleBlur,
-                  handleSubmit,
-                  isSubmitting,
-                  /* and other goodies */
-              }) => (
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="email"
-                        name="email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                    />
-                    {errors.email && touched.email && errors.email}
-                    <input
-                        type="password"
-                        name="password"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
-                    />
-                    {errors.password && touched.password && errors.password}
-                    <button type="submit" disabled={isSubmitting}>
-                        Submit
-                    </button>
-                </form>
-            )}
-        </Formik>
-    </div>
+    <Grid.Container justify="center">
+        <Grid xs={2}></Grid>
+        <Grid xs={20} justify="center">
+            <Grid.Container justify="left">
+                <Grid xs={24} justify="left">
+                    <h1>重要参数</h1>
+                </Grid>
+                <Grid xs={24} justify="left">
+                    <Input label="设置网络人数" placeholder="10"/>
+                    <Spacer w={3}/>
+                    <Input label="设置网络边长" placeholder="100" />
+                    <Spacer w={3}/>
+                    <Input label="设置循环轮数" placeholder="10000" />
+                </Grid>
+                <Spacer h={3}/>
+                <Grid xs={24} justify="left">
+                    <h1>其他参数</h1>
+                </Grid>
+                <Grid xs={24}>
+                    <Grid.Container justify="left">
+                        <Grid xs={1} justify="left">
+                            <a>推荐类型</a>
+                        </Grid>
+                        <Grid xs={22} justify="left">
+                            <Radio.Group value="2" useRow>
+                                <Radio value="2">
+                                    双向推荐
+                                </Radio>
+                                <Radio value="1">
+                                    单向推荐
+                                </Radio>
+                                <Radio value="0">
+                                    不推荐
+                                </Radio>
+                            </Radio.Group>
+                        </Grid>
+                    </Grid.Container>
+                </Grid>
+            </Grid.Container>
+        </Grid>
+        <Grid xs={2} justify="center"></Grid>
+    </Grid.Container>
 );
 
 export default Basic;
